e87d2caad1b9b0b21685a36bc0c22275b86791e9
Florian Thiévent <florianjeremias@gmail.com>
2019-12-02 16:03:12 +0100

    Implemented basic Logic to run the software

---
+++ src/main/java/ch/fhnw/edu/dist/BloomFilter.java
@@ -0,0 +1,31 @@
+package ch.fhnw.edu.dist;
+
+import ch.fhnw.edu.dist.reader.Reader;
+
+public class BloomFilter {
+
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Please specify the argument for the Path to Wordlist");
+            System.out.println("How to call: ");
+            System.out.println("   java -jar -f <path/to/wordlist>");
+            System.exit(0);
+        } else if (args.length > 2) {
+            System.out.println("Please specify the correct amount of arguments");
+            System.out.println("How to call: ");
+            System.out.println("   java -jar -f <path/to/wordlist>");
+            System.exit(0);
+        } else if (args[0].equals("-f")) {
+            System.out.println("Path is present " + args[1]);
+
+            Reader reader = new Reader(args[1]);
+            reader.readWords();
+            reader.printWordList();
+
+        } else {
+            System.out.println("An error occured. please try again");
+        }
+
+    }
+}
