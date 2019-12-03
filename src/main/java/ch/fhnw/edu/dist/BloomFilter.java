ad41e88c1b0a2b02911080930daae12937ff1c60
Roger <r.kreienbuehl@gmx.ch>
2019-12-03 15:20:57 +0100

    added constructor

--- src/main/java/ch/fhnw/edu/dist/BloomFilter.java
+++ src/main/java/ch/fhnw/edu/dist/BloomFilter.java
@@ -1,4 +1,17 @@
 package ch.fhnw.edu.dist;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 public class BloomFilter {
+
+    private Set<String> data;
+
+    public BloomFilter(Collection<String> set) {
+        data = new HashSet<>();
+        for (String s : set) {
+            data.add(s);
+        }
+    }
 }
